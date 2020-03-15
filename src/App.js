67c1a3e8a9145c146299cd4daf09bd722a9ce0d7
Hudson Holanda <hudsonholanda55@gmail.com>
2020-03-15 17:39:17 -0300

    add app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
+// const heading = <h1 className="site-heading">Hello, React</h1>
+class App extends Component {
+  state = {
+    characters: [
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    ]
+  }
+  removerCharacter = index => {
+    const { characters } = this.state;
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    })
+  }
+  handleSubmit = character => {
+    this.setState({
+      characters: [...this.state.characters, character]
+    })
+  }
+  render() {
+    return (
+      <div className="container">
+        <Table characterData={this.state.characters} removerCharacter={this.removerCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
